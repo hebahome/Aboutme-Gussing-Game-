4c542945d4ccb243fb6d9fc91d23dfc96ddaddef
hebahome <hebaaltaweel349@gmail.com>
2021-07-28 18:05:29 +0300

    function was added

--- js/app.js
+++ js/app.js
@@ -4,85 +4,150 @@ alert(`Have fun,nice to meet you here ${userName}`);
 
 
 
-let Read = prompt('Do you like reading?');
-switch(Read) {
-case 'Yes':
+function read() {
+  let read = prompt('Do you like reading?');
+  switch (read) {
+  case 'Yes':
 
-  alert('I like it as you  ^^');
-  //console.log('the user enter :' , Read);
+    alert('I like it as you  ^^');
+    //console.log('the user enter :' , read);
 
 
-  break;
-case 'No':
-  alert('I wish you have your first experience in Reading ^^');
-  //console.log('the user enter :' , Read);
+    break;
+  case 'No':
+    alert('I wish you have your first experience in Reading ^^');
+    //console.log('the user enter :' , read);
 
-  break;
-default:
-  alert('At all ,wish a good time ^^');
-        //console.log('the user enter :' , Read);
+    break;
+  default:
+    alert('At all ,wish a good time ^^');
+    //console.log('the user enter :' , read);
+  }
+
+}
+read();
+
+
+
+function music() {
+
+
+
+  let music = prompt('Do you prefer to listen to some music while you read?');
+  switch (music) {
+  case 'Yes':
+
+    alert('I like it as you  ^^');
+    //console.log('the user enter :' , music);
+
+
+    break;
+  case 'No':
+    alert('Here is an awosome one to listen which is Water Music by Frederic Handel ^^');
+    //console.log('the user enter :' , music);
+
+    break;
+  default:
+    alert('wish u have a relaxed mind while reading ^^');
+    //console.log('the user enter :' , music);
+  }
+
+}
+
+music();
+
+
+function language() {
+  let language = prompt('Do you read in other languages in addition to yours ?');
+  switch (language) {
+  case 'Yes':
+
+    alert('You are a smart person  ^^');
+
+    //console.log('the user enter :' , language);
+    break;
+  case 'No':
+    alert('I advice you to develop your skill ^^');
+    //console.log('the user enter :' , language);
+
+    break;
+  default:
+    alert('Improved  your Verbal and Spatial Abilities.^^');
+    //console.log('the user enter :' , language);
+  }
+
+
 }
 
 
-
-let music = prompt('Do you prefer to listen to some music while you read?');
-switch(music) {
-case 'Yes':
-
-  alert('I like it as you  ^^');
-  //console.log('the user enter :' , music);
+language();
 
 
-  break;
-case 'No':
-  alert('Here is an awosome one to listen which is Water Music by Frederic Handel ^^');
-  //console.log('the user enter :' , music);
+function place() {
+  let place = prompt('Have you ever read in the most beatiful library in Amman which is shoman library ?');
+  switch (place) {
+  case 'Yes':
 
-  break;
-default:
-  alert('wish u have a relaxed mind while reading ^^');
-        //console.log('the user enter :' , music);
+    alert('You Choose a good place to read at');
+
+    //console.log('the user enter :' , place);
+break;
+  case 'No':
+    alert('I advice you to visit it ^^');
+    //console.log('the user enter :' , place);
+
+    break;
+  default:
+    alert('Have fun to read where you want^^');
+    //console.log('the user enter :' , place);
+
+    alert('Welcome here' + userName);
+
+  }
+}
+place();
+
+
+
+
+function guessFav() {
+
+  let favNum = 6;
+
+  let userInput = prompt('can you guess my fav number');
+
+  for (let i = 0; i < 4; i++) {
+    //console.log(favNum + userInput);
+
+    if (favNum === Number(userInput)) {
+      alert('you guess my favourite');
+
+      //console.log(favNum + userInput);
+      break;
+    }
+
+
+    else if (Number(userInput) < favNum) {
+
+
+
+
+
+      //{console.log(favNum + userInput);
+      userInput = prompt('you type a one lower than my own');
+    }
+
+    else { userInput = prompt('you type a one  higher than my own'); }
+
+
+
+
+  }
+
 }
 
+guessFav();
 
-let language = prompt('Do you read in other languages in addition to yours ?');
-switch(language) {
-case 'Yes':
-
-  alert('You are a smart person  ^^');
-
-  //console.log('the user enter :' , language);
-  break;
-case 'No':
-  alert('I advice you to develop your skill ^^');
-  //console.log('the user enter :' , language);
-
-  break;
-default:
-  alert('Improved  your Verbal and Spatial Abilities.^^');
-        //console.log('the user enter :' , language);
-}
-
-
-let Place = prompt('Have you ever read in the most beatiful library in Amman which is shoman library ?');
-switch(Place) {
-case 'Yes':
-
-  alert('You Choose a good place to read at');
-
-  //console.log('the user enter :' , Place);
-  break;
-case 'No':
-  alert('I advice you to visit it ^^');
-  //console.log('the user enter :' , Place);
-
-  break;
-default:
-  alert('Have fun to read where you want^^');
-        //console.log('the user enter :' , Place);
-}
-
-alert('Welcome here'+ userName);
 
 
 
